1925fcb28d5b2691b4beeb0f9e084f378b43202e
Effi Ovadia <8449503+EffiOvadia@users.noreply.github.com>
2023-06-11 03:14:41 +0800

    new file:   hyper-v/ubuntu.bash 	new file:   virtualbox/ubuntu.bash 	new file:   vmware/ubuntu.bash

---
+++ hyper-v/ubuntu.bash
@@ -0,0 +1,17 @@
+#@ Change repositories to Microsoft Azure 
+sudo \cat > /etc/apt/sources.list <<-EOF
+### $(lsb_release -sd) $(lsb_release -sc)
+### --------------------------------------------------------------------------------------------------------
+deb [arch=$(dpkg --print-architecture)] http://azure.archive.ubuntu.com/ubuntu  $(lsb_release -sc)           main restricted universe multiverse
+deb [arch=$(dpkg --print-architecture)] http://azure.archive.ubuntu.com/ubuntu  $(lsb_release -sc)-updates   main restricted universe multiverse
+deb [arch=$(dpkg --print-architecture)] http://azure.archive.ubuntu.com/ubuntu  $(lsb_release -sc)-backports main restricted universe multiverse
+deb [arch=$(dpkg --print-architecture)] http://security.ubuntu.com/ubuntu       $(lsb_release -sc)-security  main restricted universe multiverse
+### --------------------------------------------------------------------------------------------------------
+EOF
+
+#@ Install optimized azure/hyper-v ubuntu kernel
+sudo apt update && apt install linux-azure
+
+#@ Setup the system to automaticly update 
+sudo apt install --assume-yes unattended-upgrades
+sudo dpkg-reconfigure --priority=low  unattended-upgrades
