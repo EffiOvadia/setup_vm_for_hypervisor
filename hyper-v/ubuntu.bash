e302916c4a07f23cdf07d48508d45f35f63d0680
Effi Ovadia <8449503+EffiOvadia@users.noreply.github.com>
2023-06-19 04:00:07 +0800

    renamed:    hyper-v/hyperv.ps1 -> hyper-v/host.ps1 	modified:   hyper-v/ubuntu.bash

--- hyper-v/ubuntu.bash
+++ hyper-v/ubuntu.bash
@@ -1,12 +1,12 @@
 #@ Change repositories to Microsoft Azure 
 sudo \cat > /etc/apt/sources.list <<-EOF
 ### $(lsb_release -sd) $(lsb_release -sc)
-### --------------------------------------------------------------------------------------------------------
-deb [arch=$(dpkg --print-architecture)] http://azure.archive.ubuntu.com/ubuntu  $(lsb_release -sc)           main restricted universe multiverse
-deb [arch=$(dpkg --print-architecture)] http://azure.archive.ubuntu.com/ubuntu  $(lsb_release -sc)-updates   main restricted universe multiverse
-deb [arch=$(dpkg --print-architecture)] http://azure.archive.ubuntu.com/ubuntu  $(lsb_release -sc)-backports main restricted universe multiverse
-deb [arch=$(dpkg --print-architecture)] http://security.ubuntu.com/ubuntu       $(lsb_release -sc)-security  main restricted universe multiverse
-### --------------------------------------------------------------------------------------------------------
+### -------------------------------------------------------------------------------------------------------
+deb [arch=$(dpkg --print-architecture)] http://azure.archive.ubuntu.com/ubuntu $(lsb_release -sc)           main restricted universe multiverse
+deb [arch=$(dpkg --print-architecture)] http://azure.archive.ubuntu.com/ubuntu $(lsb_release -sc)-updates   main restricted universe multiverse
+deb [arch=$(dpkg --print-architecture)] http://azure.archive.ubuntu.com/ubuntu $(lsb_release -sc)-backports main restricted universe multiverse
+deb [arch=$(dpkg --print-architecture)] http://security.ubuntu.com/ubuntu      $(lsb_release -sc)-security  main restricted universe multiverse
+### -------------------------------------------------------------------------------------------------------
 EOF
 
 #@ Install optimized azure/hyper-v ubuntu kernel
@@ -14,4 +14,4 @@ sudo apt update && apt install --assume-yes linux-azure
 
 #@ Setup the system to automaticly update 
 sudo apt install --assume-yes unattended-upgrades
-sudo dpkg-reconfigure --priority=low  unattended-upgrades
+sudo dpkg-reconfigure --priority=low unattended-upgrades
