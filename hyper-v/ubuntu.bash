6a3c0eca29004feacc45774c2bea00baf58efe70
Effi Ovadia <8449503+EffiOvadia@users.noreply.github.com>
2023-06-26 05:16:21 +0800

    modified:   hyper-v/ubuntu.bash

--- hyper-v/ubuntu.bash
+++ hyper-v/ubuntu.bash
@@ -1,20 +1,27 @@
-#@ Change repositories to Microsoft Azure 
-sudo \cat > /etc/apt/sources.list <<-EOF
-### $(lsb_release -sd) $(lsb_release -sc)
-### -------------------------------------------------------------------------------------------------------
-deb [arch=$(dpkg --print-architecture)] http://azure.archive.ubuntu.com/ubuntu $(lsb_release -sc)           main restricted universe multiverse
-deb [arch=$(dpkg --print-architecture)] http://azure.archive.ubuntu.com/ubuntu $(lsb_release -sc)-updates   main restricted universe multiverse
-deb [arch=$(dpkg --print-architecture)] http://azure.archive.ubuntu.com/ubuntu $(lsb_release -sc)-backports main restricted universe multiverse
-deb [arch=$(dpkg --print-architecture)] http://security.ubuntu.com/ubuntu      $(lsb_release -sc)-security  main restricted universe multiverse
-### -------------------------------------------------------------------------------------------------------
-EOF
+#!/bin/bash
 
+#@ Install TOR and HTTPS transport for apt 
+sudo apt install --assume-yes apt-transport-https apt-transport-tor 
+
+#@ delete old format sources.list 
+sudo [ -f /etc/apt/sources.list ] && sudo rm -f /etc/apt/sources.list
+
+#@ Generate new deb822 format ubuntu.sources file in sources.list.d
 sudo cat > /etc/apt/sources.list.d/ubuntu.sources <<-EOF
 X-Repolib-Name: $(lsb_release -sd) $(lsb_release -sc)
 Enabled: yes
 Types: deb deb-src
-URIs: http://azure.archive.ubuntu.com/ubuntu
-Suites: $(lsb_release -sc) $(lsb_release -sc)-updates $(lsb_release -sc)-backports $(lsb_release -sc)-security
+URIs: hhttp://azure.archive.ubuntu.com/ubuntu
+Suites: $(lsb_release -sc) $(lsb_release -sc)-updates $(lsb_release -sc)-backports
+Components: main restricted universe multiverse
+Architectures: $(dpkg --print-architecture)
+Signed-By: /usr/share/keyrings/ubuntu-archive-keyring.gpg
+
+X-Repolib-Name: $(lsb_release -sd) $(lsb_release -sc)
+Enabled: yes
+Types: deb deb-src
+URIs: http://security.ubuntu.com/ubuntu  
+Suites: $(lsb_release -sc)-security
 Components: main restricted universe multiverse
 Architectures: $(dpkg --print-architecture)
 Signed-By: /usr/share/keyrings/ubuntu-archive-keyring.gpg
