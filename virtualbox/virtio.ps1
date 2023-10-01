3228f6e1aabd55be53fb060cb09d7ad1da64817e
Effi Ovadia <8449503+EffiOvadia@users.noreply.github.com>
2023-10-01 16:25:15 +0800

    modified:   virtualbox/virtio.ps1

--- virtualbox/virtio.ps1
+++ virtualbox/virtio.ps1
@@ -2,7 +2,8 @@
 [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
 
 $URI = "https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/latest-virtio/virtio-win.iso"
-Invoke-WebRequest "$URI" -outfile .\latest-virtio-win.iso -passthru -UseBasicParsing | Select-Object -Expand headers
+
+Invoke-WebRequest $URI -outfile .\latest-virtio-win.iso -passthru -UseBasicParsing | Select-Object -Expand headers
 
 $URI = "https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/stable-virtio/virtio-win.iso"
-Invoke-WebRequest "$URI" -outfile .\stable-virtio-win.iso -passthru -UseBasicParsing | Select-Object -Expand headers
+Invoke-WebRequest $URI -outfile .\stable-virtio-win.iso -passthru -UseBasicParsing | Select-Object -Expand headers
